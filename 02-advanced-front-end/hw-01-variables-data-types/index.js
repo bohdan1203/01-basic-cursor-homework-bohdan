bbc67b60968bc2615072e733118fbf516e353c63
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2021-05-31 22:00:42 +0300

    variables have been declared

---
+++ 02-advanced-front-end/hw-01-variables-data-types/index.js
@@ -0,0 +1,7 @@
+let rollsRoycePhantom = 15.678;
+let nokia3310 = 123.965;
+let youTubePremium = 90.2345;
+
+console.log (`The price of Rolls-Royce Phantom is ${rollsRoycePhantom} UAH 
+            \nThe price of Nokia 3310 is ${nokia3310} UAH 
+            \nThe price of YouTube Premium is ${youTubePremium} UAH`);
