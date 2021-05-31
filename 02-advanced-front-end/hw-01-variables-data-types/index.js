3a8a3e1ef304fa57406db8d5085b9fcb4646f139
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2021-06-01 01:09:48 +0300

    variables names have been improved

--- 02-advanced-front-end/hw-01-variables-data-types/index.js
+++ 02-advanced-front-end/hw-01-variables-data-types/index.js
@@ -1,30 +1,30 @@
-let rollsRoycePhantom = 15.678;
-let nokia3310 = 123.965;
-let youTubePremium = 90.2345;
-console.log (rollsRoycePhantom);
-console.log(nokia3310); 
-console.log(youTubePremium);
+let rollsRoycePhantomPrice = 15.678;
+let nokia3310Price = 123.965;
+let youTubePremiumPrice = 90.2345;
+console.log(rollsRoycePhantomPrice);
+console.log(nokia3310Price); 
+console.log(youTubePremiumPrice);
 
 // Максимальне та мінімальне числа
-console.log (Math.max(rollsRoycePhantom, nokia3310, youTubePremium));
-console.log (Math.min(rollsRoycePhantom, nokia3310, youTubePremium));
+console.log(Math.max(rollsRoycePhantomPrice, nokia3310Price, youTubePremiumPrice));
+console.log(Math.min(rollsRoycePhantomPrice, nokia3310Price, youTubePremiumPrice));
 
 // Вартість всіх товарів
-let totalAmountPayable = rollsRoycePhantom + nokia3310 + youTubePremium;
-console.log (totalAmountPayable);
+let totalAmountPayable = rollsRoycePhantomPrice + nokia3310Price + youTubePremiumPrice;
+console.log(totalAmountPayable);
 
 
 // Вартість всіх товарів без копійок
-console.log (Math.trunc(rollsRoycePhantom) + Math.trunc(nokia3310) + Math.trunc(youTubePremium));
+console.log(Math.trunc(rollsRoycePhantomPrice) + Math.trunc(nokia3310Price) + Math.trunc(youTubePremiumPrice));
 
 // Вартість всіх товарів, округлена до сотень
-console.log (Math.round(totalAmountPayable / 100) * 100);
+console.log(Math.round(totalAmountPayable / 100) * 100);
 
 // Сума парна, чи не парна?
 let totalRoundedToLess = Math.floor(totalAmountPayable);
 let totalRemainder = totalRoundedToLess % 2;
-let oddOrEven = totalRemainder === 0;
-console.log (oddOrEven);
+let isItAnEvenNumber = totalRemainder === 0;
+console.log(isItAnEvenNumber);
 
 
 // Решта при оплаті (500 гривень)
@@ -68,15 +68,15 @@ console.log(netProfit);
 console.log(`++++++++++++++++ ADVANCED ++++++++++++++++`);
 
 console.log(
-    `The price of Rolls-Royce Phantom is ${rollsRoycePhantom} UAH 
-    \nThe price of Nokia 3310 is ${nokia3310} UAH 
-    \nThe price of YouTube Premium is ${youTubePremium} UAH
-    \nThe highest price of them is ${Math.max(rollsRoycePhantom, nokia3310, youTubePremium)} UAH
-    \nThe lowest price of them is ${Math.min(rollsRoycePhantom, nokia3310, youTubePremium)} UAH
+    `The price of Rolls-Royce Phantom is ${rollsRoycePhantomPrice} UAH 
+    \nThe price of Nokia 3310 is ${nokia3310Price} UAH 
+    \nThe price of YouTube Premium is ${youTubePremiumPrice} UAH
+    \nThe highest price of them is ${Math.max(rollsRoycePhantomPrice, nokia3310Price, youTubePremiumPrice)} UAH
+    \nThe lowest price of them is ${Math.min(rollsRoycePhantomPrice, nokia3310Price, youTubePremiumPrice)} UAH
     \nTotal: ${totalAmountPayable} UAH
-    \nTotal: ${(Math.trunc(rollsRoycePhantom) + Math.trunc(nokia3310) + Math.trunc(youTubePremium))} UAH
+    \nTotal: ${(Math.trunc(rollsRoycePhantomPrice) + Math.trunc(nokia3310Price) + Math.trunc(youTubePremiumPrice))} UAH
     \nTotal (rounded to hundreds): ${(Math.round(totalAmountPayable / 100) * 100)} UAH
-    \nIs ${totalRoundedToLess} an even number? ====> ${oddOrEven}
+    \nIs ${totalRoundedToLess} an even number? ====> ${isItAnEvenNumber}
     \nHere is your charge: ${(500 - totalAmountPayable)} UAH
     \nThe average amount, rounded to 2 decimal places is ${(Math.round(averageAmount * 100) / 100)} UAH
     \nThe amount is ${totalWihoutDiscount} UAH
