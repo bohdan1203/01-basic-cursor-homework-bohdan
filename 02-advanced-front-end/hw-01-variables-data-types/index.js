30638df8cd557aef18d7911bd6e67da4d6fd65ee
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2021-06-01 23:38:27 +0300

    lets containing goods' prices have been replaced with consts, variables names have been changed, several variables have been created

--- 02-advanced-front-end/hw-01-variables-data-types/index.js
+++ 02-advanced-front-end/hw-01-variables-data-types/index.js
@@ -1,21 +1,23 @@
-let rollsRoycePhantomPrice = 15.678;
-let nokia3310Price = 123.965;
-let youTubePremiumPrice = 90.2345;
+const rollsRoycePhantomPrice = 15.678;
+const nokia3310Price = 123.965;
+const youTubePremiumPrice = 90.2345;
 console.log(rollsRoycePhantomPrice);
 console.log(nokia3310Price); 
 console.log(youTubePremiumPrice);
 
 // Максимальне та мінімальне числа
-console.log(Math.max(rollsRoycePhantomPrice, nokia3310Price, youTubePremiumPrice));
-console.log(Math.min(rollsRoycePhantomPrice, nokia3310Price, youTubePremiumPrice));
+let highestPrice = Math.max(rollsRoycePhantomPrice, nokia3310Price, youTubePremiumPrice);
+let lowestPrice = Math.min(rollsRoycePhantomPrice, nokia3310Price, youTubePremiumPrice);
+console.log(highestPrice);
+console.log(lowestPrice);
 
 // Вартість всіх товарів
 let totalAmountPayable = rollsRoycePhantomPrice + nokia3310Price + youTubePremiumPrice;
 console.log(totalAmountPayable);
 
-
 // Вартість всіх товарів без копійок
-console.log(Math.trunc(rollsRoycePhantomPrice) + Math.trunc(nokia3310Price) + Math.trunc(youTubePremiumPrice));
+let totalWithoutChangeCoins = Math.trunc(rollsRoycePhantomPrice) + Math.trunc(nokia3310Price) + Math.trunc(youTubePremiumPrice);
+console.log(totalWithoutChangeCoins);
 
 // Вартість всіх товарів, округлена до сотень
 console.log(Math.round(totalAmountPayable / 100) * 100);
@@ -23,12 +25,12 @@ console.log(Math.round(totalAmountPayable / 100) * 100);
 // Сума парна, чи не парна?
 let totalRoundedToLess = Math.floor(totalAmountPayable);
 let totalRemainder = totalRoundedToLess % 2;
-let isItAnEvenNumber = totalRemainder === 0;
-console.log(isItAnEvenNumber);
-
+let isEvenNumber = totalRemainder === 0;
+console.log(isEvenNumber);
 
 // Решта при оплаті (500 гривень)
-console.log(500 - totalAmountPayable);
+let clientsPayment = 500;
+console.log(clientsPayment - totalAmountPayable);
 
 // Cереднє значення цін, округлене до другого знаку після коми
 let averageAmount = totalAmountPayable / 3;
@@ -38,11 +40,11 @@ console.log(Math.round(averageAmount * 100) / 100);
 ///////////// ЗАДАЧА /////////////
 
 // Вартість без знижки
-let totalWihoutDiscount = (Math.round(totalAmountPayable * 100) / 100);
-console.log(totalWihoutDiscount);
+let totalWithoutDiscount = (Math.round(totalAmountPayable * 100) / 100);
+console.log(totalWithoutDiscount);
 
 // Собівартість
-let costPrice = totalWihoutDiscount / 2;
+let costPrice = totalWithoutDiscount / 2;
 console.log(costPrice);
 
 // Випадкова знижка
@@ -50,17 +52,16 @@ let discount = Math.round(Math.random() * 100);
 console.log(discount);
 
 // Сума знижки
-let discountAmount = Math.round((totalWihoutDiscount / 100 * discount) * 100) / 100;
+let discountAmount = Math.round((totalWithoutDiscount / 100 * discount) * 100) / 100;
 console.log(discountAmount);
 
 // Сума до сплати
-let amountToBePaid = Math.round((totalWihoutDiscount - discountAmount) * 100) / 100;
+let amountToBePaid = Math.round((totalWithoutDiscount - discountAmount) * 100) / 100;
 console.log(amountToBePaid);
 
 // Чистий прибуток
 let netProfit = Math.round((costPrice - discountAmount) * 100) / 100;
 console.log(netProfit);
-
 
 
 ///////////// ADVANCED /////////////
@@ -71,15 +72,15 @@ console.log(
     `The price of Rolls-Royce Phantom is ${rollsRoycePhantomPrice} UAH 
     \nThe price of Nokia 3310 is ${nokia3310Price} UAH 
     \nThe price of YouTube Premium is ${youTubePremiumPrice} UAH
-    \nThe highest price of them is ${Math.max(rollsRoycePhantomPrice, nokia3310Price, youTubePremiumPrice)} UAH
-    \nThe lowest price of them is ${Math.min(rollsRoycePhantomPrice, nokia3310Price, youTubePremiumPrice)} UAH
+    \nThe highest price of them is ${highestPrice} UAH
+    \nThe lowest price of them is ${lowestPrice} UAH
     \nTotal: ${totalAmountPayable} UAH
-    \nTotal: ${(Math.trunc(rollsRoycePhantomPrice) + Math.trunc(nokia3310Price) + Math.trunc(youTubePremiumPrice))} UAH
+    \nTotal: ${totalWithoutChangeCoins} UAH
     \nTotal (rounded to hundreds): ${(Math.round(totalAmountPayable / 100) * 100)} UAH
-    \nIs ${totalRoundedToLess} an even number? ====> ${isItAnEvenNumber}
-    \nHere is your charge: ${(500 - totalAmountPayable)} UAH
+    \nIs ${totalRoundedToLess} an even number? ====> ${isEvenNumber}
+    \nHere is your charge: ${(clientsPayment - totalAmountPayable)} UAH
     \nThe average amount, rounded to 2 decimal places is ${(Math.round(averageAmount * 100) / 100)} UAH
-    \nThe amount is ${totalWihoutDiscount} UAH
+    \nThe amount is ${totalWithoutDiscount} UAH
     \nThe cost price of all goods is ${costPrice} UAH
     \nThe discount is ${discount}%
     \nThe discount amount is ${discountAmount} UAH
