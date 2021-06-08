44e11875b8969b522128b5b2e7efb5143a258623
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2021-06-08 03:47:03 +0300

    8 functions have been done

--- 02-advanced-front-end/hw-03-functions/index.js
+++ 02-advanced-front-end/hw-03-functions/index.js
@@ -1,3 +1,5 @@
+/*
+
 // 1. Функція, яка тримує будь-яке число та виводить найбільшу цифру в цьому числі
 function getMaxDigit() {
     const number = prompt(`Введіть велике число: `, 2131237235135676);
@@ -154,3 +156,36 @@ function convertCurrency() {
     document.writeln(resultString);
 }
 convertCurrency();
+
+*/
+
+// Генерація випадкового пароля
+function getRandomPassword(password = Math.round(Math.random() * 100000000)) {
+
+    // Чи влаштовує користувача дефолтна довжина пароля (8 чисел)?
+    let isSuitable = confirm(`Чи влаштовує Вас пароль з 8 чисел?`);
+
+    // Якщо так
+    if (isSuitable) {
+        alert(`Ваш пароль: ${password}`)
+        console.log(password)
+        return;
+        // Якщо ні...
+    }   else {
+        // Запитуємо, скільки чисел він хоче
+        let digitsQuantity = +prompt('Встановіть бажану довжину пароля (кількість чисел):', 6);
+
+        // Пароль не має бути занадто коротким або занадто довгим
+        if (digitsQuantity < 4 || digitsQuantity > 16) {
+            alert(`Пароль має містити не менше 4 і не більше 16 чисел`);
+            return
+        } 
+
+        digitsQuantity = 10 ** digitsQuantity
+        password = Math.round(Math.random() * digitsQuantity);
+
+        console.log(password)
+    }
+}
+getRandomPassword();
+
