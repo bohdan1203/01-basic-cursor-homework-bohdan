d24771ca94817a1485e9d1c7f60154f1e54be2be
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2021-06-28 23:02:06 +0300

    q

--- 02-advanced-front-end/hw-08-classes/index.js
+++ 02-advanced-front-end/hw-08-classes/index.js
@@ -1,20 +1,17 @@
 const marks = [5, 4, 4, 5];
 
-// 1
 class Student {
     constructor(university, course, fullName) {
         this.university = university;
         this.course = course;
         this.fullName = fullName;
 
-        // 2
         this.getInfo = function() {
             return `Студент ${course}го курсу, ${university}, ${fullName}`;
         };
 
         this.marks = marks;
 
-        // 5
         this.averageMark = function() {
             let sum = 0;
 
@@ -24,23 +21,19 @@ class Student {
             return this.averageMark = sum / this.marks.length;
         };
 
-        // 6
         this.dismiss = function() {
             return this.marks = null;
         };
 
-        // 7
         this.recover = function() {
             return this.marks = marks;
         };
     };
 
-    // 3
     get getMarks() {
         return this.marks;
     };
 
-    // 4
     set setMarks(mark) {
         if (this.marks == null) return null;
         return this.marks.push(mark);
@@ -48,7 +41,6 @@ class Student {
 };
 
 const ostap = new Student('Вища Школа Психотерапії м.Одеса', 2, 'Остап Бендер');
-
 
 console.log(ostap.getInfo());
 
@@ -65,36 +57,3 @@ ostap.setMarks = 5;
 console.log(ostap.getMarks);
 
 console.log(ostap.recover());
-
-
-
-// console.log(`////////////////////////////////ADVANCED////////////////////////////////`);
-
-// class BudgetStudent extends Student {
-//     constructor(university, course, fullName) {
-//         super(university, course, fullName);
-
-//         this.getScholarship = function() {
-//             if (this.averageMark >= 4) return `Ви отримали 1400 грн. стипендії`;
-//             else return `Ви не отримаєте стипендію`;
-//         };
-
-//         this.interval = setInterval(this.getScholarship, 2000);
-//     };
-
-
-// };
-
-// const theodore = new BudgetStudent('УУУ', 1, 'Теодор Рузвельт');
-
-
-// console.log(theodore.marks);
-// console.log(theodore.averageMark());
-// console.log(theodore.getScholarship());
-
-// console.log(theodore.interval);
-
-
-
-
-
