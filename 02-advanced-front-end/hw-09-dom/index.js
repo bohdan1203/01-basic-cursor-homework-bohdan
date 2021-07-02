ce9e2110621bb8cd5370791c084ceb10c993c4d0
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2021-07-02 23:51:59 +0300

    homework has been improved

--- 02-advanced-front-end/hw-09-dom/index.js
+++ 02-advanced-front-end/hw-09-dom/index.js
@@ -7,16 +7,12 @@ function generateBlocks() {
         let r = Math.round(Math.random() * 255);
         let g = Math.round(Math.random() * 255);
         let b = Math.round(Math.random() * 255);
-        
         let color = `rgb(${r}, ${g}, ${b})`;
 
         document.getElementById(`cell_${i}`).style.background = color;
     };
-
-    function generateBlocksInterval() {
-        document.location.reload();
-    };
-    setInterval(generateBlocksInterval, 1000);
-
 };
-generateBlocks();
+
+function generateBlocksInterval() {
+    setInterval(generateBlocks, 1000);
+};
