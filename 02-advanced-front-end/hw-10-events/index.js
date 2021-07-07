20e49b7ea20e1cfe4e8b63839396b976826a2a52
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2021-07-07 17:40:07 +0300

    function playOnClick() has been added

--- 02-advanced-front-end/hw-10-events/index.js
+++ 02-advanced-front-end/hw-10-events/index.js
@@ -7,6 +7,26 @@ document.addEventListener("keydown", function (e) {
     key.classList.add('playing');
 });
 
+function playOnClick() {
+
+    for (i = 0; i < document.getElementsByClassName('key').length; i++ ) {
+
+        let keyId = document.getElementsByClassName('key')[i].getAttribute('id');
+        let idNumber = keyId.replace('key-', '');
+
+        document.querySelector(`#${keyId}`).addEventListener("click", function() {
+
+            const audio = document.querySelector(`audio[data-key="${idNumber}"]`);
+            const key = document.querySelector(`.key[data-key="${idNumber}"]`);
+
+            audio.currentTime = 0;
+            audio.play();
+            key.classList.add('playing');
+        });
+    };
+};
+playOnClick();
+
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     this.classList.remove('playing');
@@ -17,31 +37,6 @@ const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 
-function playOnClick() {
-
-
-    for (i = 0; i < document.getElementsByClassName('key').length; i++ ) {
-
-        let keyId = document.getElementsByClassName('key')[i].getAttribute('id')
-
-        let idNumber = keyId.replace('key-', '')
-
-
-      
-
-        document.querySelector(`#${keyId}`).addEventListener("click", function() {
-
-            const audio = document.querySelector(`audio[data-key="${idNumber}"]`);
-            const key = document.querySelector(`.key[data-key="${idNumber}"]`);
-
-            audio.currentTime = 0;
-            audio.play()
-            key.classList.add('playing');
-     
-        })
-    }
-}
-playOnClick()
 
 
 
