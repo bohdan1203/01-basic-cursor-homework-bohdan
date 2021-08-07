608b91cf538fcd2db33b383ba2f41810cb837789
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2021-08-07 17:40:56 +0300

    homework #12 has been modified

--- 02-advanced-front-end/hw-12-api/index.js
+++ 02-advanced-front-end/hw-12-api/index.js
@@ -35,6 +35,11 @@ function getCharactersInformation() {
   
     charactersHeader.insertAdjacentHTML('afterbegin', `Film ${input.value}, characters:`);
 
+    if (input.value < 1 || input.value > 6) {
+        charactersContainer.insertAdjacentHTML('afterbegin', `<p class="no-film">There is no such a film. <br> You should input number in the range 1 to 6.</p>`)
+        return;
+    }
+
     fetch(`https://swapi.dev/api/films/${input.value}/`)
 
     .then((response) => {
